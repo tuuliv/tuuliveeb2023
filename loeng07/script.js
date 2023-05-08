8f803f05d7670b087bf92cdc6a80c5043bfb1b3a
Tuuli <tuulivahtra@hotmail.com>
2023-05-08 21:59:59 +0300

    matkaklubi

---
+++ loeng07/script.js
@@ -0,0 +1,14 @@
+
+console.log("tere maailm");
+console.log(123+123);
+
+let eesnimi = "Tuuli";
+let perenimi = "Vahtra";
+
+function tervitus (muutuja1, muutuja2) {
+    console.log(eesnimi + "" + perenimi);
+}
+
+tervitus("Mihkel, "Väli");
+
+alert("tere maailm!");
