88b59736702f48daddbaaa84b01c3c5fbd95c3ea
Tuuli <tuulivahtra@hotmail.com>
2023-05-19 17:32:38 +0300

    matkaklupi update 19.05.2023
    matkaklubi

---
+++ loeng10/script.js
@@ -0,0 +1,8 @@
+let inputHtml = document.querySelector("#input");
+
+inputHtml.addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+
+        console.log ("vajutati enter: )
+    }
+}
